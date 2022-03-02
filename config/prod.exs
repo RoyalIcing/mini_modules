5e129a40aa9c32f5e2797edc9834bd51aadbd7c2
Patrick Smith <pgwsmith@gmail.com>
2022-03-02 23:10:55 +1100

    Move to runtime

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,6 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :mini_modules, MiniModulesWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
