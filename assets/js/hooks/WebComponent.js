6479c6abb302fd789b20d16e4660c95e005d62e2
Patrick Smith <pgwsmith@gmail.com>
2022-03-09 13:27:43 +1100

    Use abort signal

--- assets/js/hooks/WebComponent.js
+++ assets/js/hooks/WebComponent.js
@@ -1,5 +1,7 @@
 export const WebComponentHook = Object.freeze({
     mounted() {
+        this.aborter = new AbortController();
+        const signal = this.aborter.signal;
         console.log("WebComponentHook", this.el);
 
         const form = this.el.closest("form");
@@ -8,10 +10,14 @@ export const WebComponentHook = Object.freeze({
             console.log(Array.from(event.formData.keys()));
             // console.log("formdata", this.el.editor);
             event.formData.append('input', editor.getModel().getValue());
-        });
+        }, { signal });
 
         // this.el.addEventListener("input", (event) => {
         //     console.log("handleEvent", event);
         // });
-    }
+    },
+
+    destroyed() {
+        this.aborter.abort();
+    },
 });
